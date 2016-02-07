b20fa02e456753f414efaf4afecd915bafa8be95
Brian Woestman <woestmab@gmail.com>
2016-02-06 21:33:52 -0600

    Converted to singleton and added EditText

---
+++ Labs/2.1/BWoestmanLab2.1/app/src/main/java/com/woestmanBrian/bWoestmanLab2_1/fragments/Ipsum.java
@@ -0,0 +1,37 @@
+package com.woestmanBrian.bWoestmanLab2_1.fragments;
+
+/**
+ * Created by Brian Woestman on 2/6/16.
+ * Android Programming
+ * MoWe 2:30p - 4:20p
+ */
+public class Ipsum {
+
+    private String title;
+    private String content;
+
+    public Ipsum() {
+    }
+
+    public Ipsum(String title, String content) {
+        this();
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
